88464ad64677b8a6454d0006bc6c251f880fb137
mehedihassanome <omemehedihassan@gmail.com>
2025-10-23 01:10:57 +0600

    Update config for renamed repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,12 +23,12 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/datanalytics/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: '/datanalytics/', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
